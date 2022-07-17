7800d2d3d6a769fe9885406aefdfe4065822e907
Joseph Chen <ooo93301@gmail.com>
2022-07-17 18:57:30 +0100

    add icon

--- satds_fx/src/main/java/fx/satds_fx/Main.java
+++ satds_fx/src/main/java/fx/satds_fx/Main.java
@@ -4,6 +4,7 @@ import classifier.trainer.Trainer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,6 +18,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("SATDSweeper");
+        stage.getIcons().add( new Image(System.getProperty("user.dir") +"\\satds_fx\\src\\main\\resources\\fx\\satds_fx\\Icon.png"));
         primary = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("input.fxml"));
         Scene scene = new Scene( fxmlLoader.load() );
